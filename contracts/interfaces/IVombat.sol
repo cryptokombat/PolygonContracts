d81458070b622b15ec1f0eba2d0ec0e91fd84cbf
Andrew U <tmyx0m0p@gmail.com>
2021-11-21 21:37:59 +0300

    add vombat interface

---
+++ contracts/interfaces/IVombat.sol
@@ -0,0 +1,22 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+
+interface IVombat is IERC20 {
+    function mint(address to, uint256 amount) external;
+
+    function burn(uint256 amount) external;
+
+    function burnFrom(address account, uint256 amount) external;
+
+    function permit(
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external;
+}
