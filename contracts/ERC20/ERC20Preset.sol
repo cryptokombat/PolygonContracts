d83012bee9883560bb23b5955ed9588f0cf1d7a6
Andrew U <tmyx0m0p@gmail.com>
2022-05-16 09:06:11 +0300

    use SafeErc20 safeTransfer to recover token

--- contracts/ERC20/ERC20Preset.sol
+++ contracts/ERC20/ERC20Preset.sol
@@ -5,8 +5,11 @@ import '@openzeppelin/contracts/access/AccessControl.sol';
 import '@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol';
 import '@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol';
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 
 contract ERC20Preset is AccessControl, ERC20Burnable, ERC20Permit {
+    using SafeERC20 for IERC20;
+
     bytes32 public constant MINTER_ROLE = keccak256('MINTER_ROLE');
 
     constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) ERC20Permit(_name) {
@@ -16,8 +19,7 @@ contract ERC20Preset is AccessControl, ERC20Burnable, ERC20Permit {
 
     function recoverToken(address token, uint256 amount) external virtual {
         require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), '!access');
-        require(IERC20(token).balanceOf(address(this)) >= amount, '!balance');
-        IERC20(token).transfer(_msgSender(), amount);
+        IERC20(token).safeTransfer(_msgSender(), amount);
     }
 
     function _msgSender() internal view override returns (address sender) {
