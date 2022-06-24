d093757b995af833571c0a10049e3114d3dd1166
Andre P <10810328+myx0m0p@users.noreply.github.com>
2022-06-24 11:28:16 +0300

    update deploy scripts and redeploy

--- deploy/004_deploy_collection_bsc.ts
+++ deploy/004_deploy_collection_bsc.ts
@@ -48,4 +48,4 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 }
 
 export default func
-func.tags = ['CollectionBsc']
+func.tags = ['CollectionBsc', 'Collections']
